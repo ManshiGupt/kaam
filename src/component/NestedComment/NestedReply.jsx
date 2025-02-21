ce5bbb7a6b63e3234e52bb761d592e8007bcc345
ManshiGupt <heymanshigupta@gmail.com>
2025-02-21 17:21:55 +0530

    jh

--- src/component/NestedComment/NestedReply.jsx
+++ src/component/NestedComment/NestedReply.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const NestedReply = ({data}) => {
+const NestedReply = ({data,i}) => {
+  console.log(data);
   return (
     <div>
-        <div className='border-solid border-2 border-black p-4'>
+        <div key={i} className='border-solid border-2 border-black p-4'>
+        <img
+            className="w-16 p-2 rounded-full"
+            src="https://www.redditstatic.com/avatars/defaults/v2/avatar_default_1.png"
+            alt="user"
+          />
             <h3>{data.username}</h3>
-            <h3 className='py-2'>{data.comment}</h3>
+            <h3 className='py-2'>{data.comment}</h3>                         
         </div>
       {data?.replies && <NestedReply data={data.replies}/>}
     </div>
