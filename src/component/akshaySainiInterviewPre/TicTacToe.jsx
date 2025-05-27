cb99175375bcfc7112138e5ae61d341a0bdfbfb4
ManshiGupt <heymanshigupta@gmail.com>
2025-05-27 16:19:31 +0530

    ko

--- src/component/akshaySainiInterviewPre/TicTacToe.jsx
+++ src/component/akshaySainiInterviewPre/TicTacToe.jsx
@@ -1,55 +1,31 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const TicTacToe = () => {
-  const [turn, setTurn] = useState({});
-  const [ram, setRam] = useState(true);
-  const[currentIndex,setCurrentIndex]=useState(1);
-  const[value,setValue]= useState()
+  const[player,setPlayer]= useState(false);
+  const[turn,setTurn]= useState({});
+  const[current,setCurrent]= useState()
 
-  const handlefun = (i) => {
-    setCurrentIndex(i)
-    if (i % 2 !== 0) {
-      setRam(false);
-    }
-    // if (i == 0) {
-    //   setTurn({ [i]: ram });
-    // } else {
-      setTurn((prev) => ({...prev, [i]:ram}));
-    // }
-    setValue(turn.i)
-
-    console.log("exi", i);
-    console.log("ram", ram);
-    console.log("turn", turn);
-    console.log("currentIndex",currentIndex)
-    console.log("turn.i",turn[i])
-  };
+  const handle=(i)=>{
+    setCurrent([i])
+    setPlayer(!player);
+    setTurn(({...turn, [i]:player}))
+    console.log(turn);
+  }
   return (
-    <div>
-      TicTacToe
-      <h1>Winner
-
-      </h1>
-      {/* <h1>Next Player<span>{turn}</span></h1> */}
+    <div>TicTacToe
+      <h1>Winner</h1>
+      <h1>Next PLayer {player?"X":"O"}</h1>
       <div>
-        <div className="flex">
-          {Array.from({ length: 3 }).map((item, i) => (
-            <div key={i}>
-              <div
-                className="border-2 bg-black text-white  flex justify-center rounded-xl p-4"
-                onClick={() => handlefun(i+1)}
-              >
-                {turn[currentIndex]}
-             
-                {console.log(currentIndex)}
-                {/* {console.log(turn[currentIndex])} */}
-              </div>
+        <div className=' gap-10 justify-center'>
+          {Array.from({length:9}).map((item,i)=>(
+            <div key={i} className=' hover:bg-red-500 p-4 rounded-xl' onClick={()=>handle(i)}>
+            {turn?(turn[i]?"X":"O"): "M "}
             </div>
           ))}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TicTacToe;
+export default TicTacToe
