83ddd53e2b79a0823ac682e6c46797f2fcd3e91b
Manshi <heymanshigupta@gmail.com>
2025-05-31 08:59:50 +0530

    lojihg

--- src/component/InfiniteScroll1.jsx
+++ src/component/InfiniteScroll1.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const InfiniteScroll1 = () => {
-  return (
-    <div>InfiniteScroll1</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const toShow = 3;
+  const [skip, setSkip] = useState(0);
+  const[total,setTotal]= useState(0)
 
-export default InfiniteScroll1
+  const fetchData = async () => {
+    const res = await axios.get(
+      `https://dummyjson.com/products?skip=${skip}&limit=10`
+    );
+    setData(res.data.products);
+  };
+  useEffect(() => {
+    fetchData();
+  }, [skip]);
+
+  const handleNext = () => {
+    setSkip((prev) => prev + toShow);
+  };
+
+  return (
+    <div>
+      <h1>InfiniteScroll1</h1>
+      <div>
+        {data.map((item, i) => (
+          <div key={i}>
+            {item.title}
+            {item.id}
+
+            <img src={item.images} className="h-40 w-40" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteScroll1;
