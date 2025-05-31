13e97f11d0b5fd9a20a38e784f29b571e078fddf
ManshiGupt <heymanshigupta@gmail.com>
2025-05-31 15:21:13 +0530

    lp

--- src/component/InfiniteScroll1.jsx
+++ src/component/InfiniteScroll1.jsx
@@ -1,24 +1,49 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const InfiniteScroll1 = () => {
   const [data, setData] = useState([]);
-  const toShow = 3;
+  const limit = 3;
   const [skip, setSkip] = useState(0);
-  const[total,setTotal]= useState(0)
-
+  const [total, setTotal] = useState();
+  const reff = useRef(null);
+  const timer =  useRef(false);
   const fetchData = async () => {
     const res = await axios.get(
-      `https://dummyjson.com/products?skip=${skip}&limit=10`
+      `https://dummyjson.com/products?skip=${skip}&limit=${limit}`
     );
-    setData(res.data.products);
+
+    setData((prev) => [...prev, ...res.data.products]);
+    setTotal(res.total);
+  };
+ 
+  const handleSome = () => {
+    console.log("chanala",timer)
+    if (timer.current)return;
+    timer.current = true;
+    setTimeout(() => {
+      if (reff.current?.getBoundingClientRect().bottom <= window.innerHeight) {
+        fetchData();
+        console.log(
+          "handleSome",
+          reff.current?.getBoundingClientRect().bottom <= window.innerHeight
+        );
+        setSkip((prev) => prev + limit);
+      }
+    }, 0);
+    console.log("chanala",timer)
   };
   useEffect(() => {
     fetchData();
-  }, [skip]);
+    setSkip((prev) => prev + limit);
+
+    window.addEventListener("scroll", handleSome);
+
+    return () => window.removeEventListener("scroll", handleSome);
+  }, []);
 
   const handleNext = () => {
-    setSkip((prev) => prev + toShow);
+    setSkip((prev) => prev + limit);
   };
 
   return (
@@ -31,9 +56,12 @@ const InfiniteScroll1 = () => {
             {item.id}
 
             <img src={item.images} className="h-40 w-40" />
+           
           </div>
         ))}
       </div>
+      <div ref={reff}></div>
+      {/* <button onClick={()=>handleNext()}>load more</button> */}
     </div>
   );
 };
