3e40059cf94979c28517fc265fc6f66874035bb6
ManshiGupt <heymanshigupta@gmail.com>
2025-06-23 17:58:03 +0530

    ko

--- src/component/flipkart/CalenderDate.jsx
+++ src/component/flipkart/CalenderDate.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 const CalenderDate = () => {
   const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  const dates = [1, 2, 3, 4, 5, 6, 7];
-  const [event,setEvent]= useState(Array.fr)
+  const [dates,setDates] = [1, 2, 3, 4, 5, 6, 7];
+  const [event, setEvent] = useState(Array.from({ length: 7 }));
+  const[setIndex,c]
   const times = [
     "1AM",
     "2AM",
@@ -39,6 +40,15 @@ const CalenderDate = () => {
     "1AM",
     "1AM",
   ];
+  const handleEvent=(i,time)=>{
+    showModal(true)
+  }
+  const filterDatePrev=()=>{
+    setDates()
+  }
+  const filterDateNext=()=>{
+    setDates()
+  }
   return (
     <div>
       {/* CalenderDate */}
@@ -47,43 +57,45 @@ const CalenderDate = () => {
           <div className=" flex justify-between">
             <div className=" text-5xl">MAY 2018</div>
             <div className=" flex ">
-              <button className="border-2 border-white rounded-md my-10 px-4">
+              <button className="border-2 border-white rounded-md my-10 px-4" onClick={()=>filterDatePrev()}>
                 prev
               </button>
               <h1 className="pt-10 px-4">Today</h1>
-              <button className="border-2 border-white rounded-md my-10 px-4">
+              <button className="border-2 border-white rounded-md my-10 px-4" onClick={()=>filterDateNext()}>
                 next
               </button>
             </div>
           </div>
           <div className="grid grid-cols-7 justify-center pl-20">
-          
             {days.map((day, i) => (
               <div className="border-2 border-white">{day}</div>
             ))}
-            
+
             {dates.map((date, i) => (
               <div className="   ">{date}</div>
             ))}
-            
           </div>
         </div>
 
-        <div className="p-4">
-            {times.map((time,i)=>(
-                <div>
-                    {time}
-                    </div>
+        <div className="p-4 grid grid-cols-8 border-2 border-black pl-40">
+          <div>
+            {times.map((time, index) => (
+              <div className="flex">
+                {time}
+                {event.map((item, i) => (
+                  <div className=" border-2 border-black p-28" onClick={()=>handleEvent(i,time)}>{/* {i} */}</div>
+                ))}
+              </div>
             ))}
-            <div className="grid grid-cols-7 border-2 border-black">
-               {event.map((item,i)=>(
-                <div>
-                    {i}
+          </div>
+          {/* <div className="grid grid-cols-7 border-2 border-black"> */}
+          {/* {event.map((item,i)=>(
+                <div className=" border-2 border-black p-20">
+                  
                     </div>
-               ))}
-                        
-                
-            </div>
+               ))} */}
+
+          {/* </div> */}
         </div>
       </div>
     </div>
